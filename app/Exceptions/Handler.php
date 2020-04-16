bec29f0603afe5ea851a41bed07407f75ce5a99d
QuangHuy <huybq@vicoders.com>
2020-04-16 15:17:23 +0700

    larravel 6 handle

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Exception;
 
 class Handler extends ExceptionHandler
 {
@@ -34,7 +34,7 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function report(Throwable  $exception)
+    public function report(Exception  $exception)
     {
         parent::report($exception);
     }
@@ -48,7 +48,7 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception)
+    public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
     }
