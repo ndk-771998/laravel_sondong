9ac8c92354412e3d95ef236762c5af0bd5bc708e
Nguyen Nhat Linh <linhnn@vmms.vn>
2020-11-11 11:10:26 +0700

    fix: add HasCategoriesTrait,HasTagTrait

--- app/Entities/Post.php
+++ app/Entities/Post.php
@@ -3,12 +3,14 @@
 namespace App\Entities;
 
 use Illuminate\Support\Str;
+use VCComponent\Laravel\Category\Traits\HasCategoriesTrait;
 use VCComponent\Laravel\Comment\Traits\HasCommentTrait;
 use VCComponent\Laravel\Post\Entities\Post as BasePost;
+use VCComponent\Laravel\Tag\Traits\HasTagsTraits;
 
 class Post extends BasePost
 {
-    use HasCommentTrait;
+    use HasCommentTrait , HasCategoriesTrait, HasTagsTraits;
 
     public function postTypes()
     {
