9f5e3770678a01e0682cf940931625b3d98ef518
hieunn <hieunn@vicoders.com>
2021-10-22 11:21:29 +0700

    refactor: setting config

--- config/settings.php
+++ config/settings.php
@@ -4,7 +4,7 @@ return [
     'auth_middleware' => [
         'admin'   => [
             [
-                'middleware' => 'jwt.auth',
+                'middleware' => null,
                 'except'     => [],
             ],
         ],
