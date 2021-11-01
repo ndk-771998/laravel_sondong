2b0b5c0fca9281ede808d0c86296cb7911888242
unknown <nguyenduykhanh827@gmail.com>
2021-11-01 11:17:19 +0700

    style header responsive

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,37 +1,93 @@
 <header>
     <div class="header">
-        <div class="header-left">
-            <a href=""><img src="{{ url('/image/logo.png') }}" al♣t=""></a>
-            <p>Trung tâm văn hoá - Thông tin và Thể thao <br>Sơn Động, Bắc Giang</p>
-        </div>
-        <div class="header-middle">
-            <a href=""> <i class="fa fa-volume-up" aria-hidden="true"></i> Chương trình truyền thanh</a>
-            <a href=""> <i class="fa fa-play-circle" aria-hidden="true"></i> Video truyền hình</a>
-        </div>
-        <div class="header-right">
-            <div class="time">
-                <h3> <span>13:51 </span> Thứ năm, 18 tháng 3 năm 2021</h3>
+        <div class="container _header">
+            <div class="row">
+            <div class="header-left col-12 col-md-4 col-sm-12">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-12 image">
+                        <a href=""><img src="{{ url('/image/logo.png') }}" alt=""></a>
+                    </div>
+                    <div class="col-md-9 name">
+                        <a class=" d-none d-md-block" href="">Trung tâm văn hoá - Thông tin và Thể thao Sơn Động, Bắc Giang</a>
+                    </div>
+                </div>
             </div>
-            <div class="form">
-                <form action="">
-                    <input type="text">
-                    <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-                </form>
+            <div class="header-middle d-none d-md-block col-12 col-md-5 col-sm-12">
+                <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <a href=""> <i class= "fa fa-volume-up"></i> Chương trình truyền thanh</a>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <a href=""> <i class= "fa fa-play-circle"></i> Video truyền hình</a>
+                </div>
+                </div>
+            </div>
+            <div class="header-right col-xs-12 col-md-3 col-sm-12">
+                <div class="time">
+                    <h3> <span>13:51 </span> Thứ năm, 18 tháng 3 năm 2021</h3>
+                </div>
+                <div class="form">
+                    <form action="">
+                        <input type="text">
+                        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                    </form>
+                </div>
+            </div>
             </div>
         </div>
     </div>
-    <div class="menu">
-        <div class="_menu">
-            <ul>
-                <li><a class="home" href="#"><i class="fa fa-fw fa-home"></i></a></li>
-                <li class="active"><a href="#">Giới thiệu</a></li>
-                <li><a class="" href="#">Tin chính trị</a></li>
-                <li><a class="" href="#">Văn hoá - xã hội</a></li>
-                <li><a class="" href="#">Kinh kế</a></li>
-                <li><a class="" href="#">Quốc phòng an ninh</a></li>
-                <li><a class="" href="#">Pháp luật</a></li>
-                <li><a class="" href="#">Sức khoẻ</a></li>
-            </ul>
+    <div class="menu col-md-12 p-0">
+            <div class="_menu container">
+                <div id="toggle">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <nav id="nav" class="nav navbar">
+                    <ul class="nav p-0">
+                        <div>
+                            <li class="nav-item"><a class="home" href="#"><i class="fa fa-fw fa-home"></i></a></li>
+                        </div>
+                        <div class="dropdown">
+                            <li class="active dropbtn"><a href="#">Giới thiệu</a></li>
+                            <div class="dropdown-content">
+                                <li>
+                                    <a href="">Link 1</a>
+                                </li>
+                                <li>
+                                    <a href="">Link 1</a>
+                                </li>
+                                <li>
+                                    <a href="">Link 1</a>
+                                </li>
+                            </div>
+                        </div>
+                        <div>
+                            <li class="nav-item"><a class="" href="#">Tin chính trị</a></li>
+                        </div>
+                        <div>
+                            <li class="nav-item"><a class="" href="#">Văn hoá - xã hội</a></li>
+                        </div>
+                        <div>
+                            <li class="nav-item"><a class="" href="#">Kinh kế</a></li>
+                        </div>
+                        <div>
+                            <li class="nav-item"><a class="" href="#">Quốc phòng an ninh</a></li>
+                        </div>
+                        <div>
+                            <li class="nav-item"><a class="" href="#">Pháp luật</a></li>
+                        </div>
+                        <div>
+                            <li class="nav-item"><a class="" href="#">Sức khoẻ</a></li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
         </div>
-    </div>
 </header>
+
+<script>
+        $(document).ready(function(){
+            $('#toggle').click(function(){
+                $('#nav').slideToggle();
+            });
+        })
+    </script>
