41006c6cdbfcb50f32351ff3742ad5264dc58ef3
unknown <nguyenduykhanh827@gmail.com>
2021-11-04 15:13:45 +0700

    Style Document List and Document Detail

--- resources/views/include/post/pagination.blade.php
+++ resources/views/include/post/pagination.blade.php
@@ -4,11 +4,19 @@
         <li class="page-item first"><a class="" href="#"><< Đầu</a></li>
         <li class="page-item previous"><a class="" href="#">< Trước</a></li>
         <li class="normal">...</li>
-        <li class="page-item"><a class="" href="#">7</a></li>
+        <!-- <li class="page-item"><a class="" href="#">7</a></li>
         <li class="page-item active"><a class="" href="#">8</a></li>
         <li class="page-item"><a class="" href="#">9</a></li>
         <li class="page-item"><a class="" href="#">10</a></li>
-        <li class="page-item"><a class="" href="#">11</a></li>
+        <li class="page-item"><a class="" href="#">11</a></li> -->
+        @for($i=7; $i<=11;$i++)
+            @if($i==8)
+                <li class="page-item {{'active'}}"><a class="" href="#">{{$i}}</a></li>
+                @else
+                <li class="page-item"><a class="" href="#">{{$i}}</a></li>
+            @endif
+
+        @endfor
         <li class="normal">...</li>
         <li class="page-item next"><a class="" href="#">Sau ></a></li>
         <li class="page-item last"><a class="" href="#">Cuối >></a></li>
