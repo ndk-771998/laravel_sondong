3137d609d1d5f22aa172b97be8dec7fb84143f87
unknown <nguyenduykhanh827@gmail.com>
2021-11-04 13:32:38 +0700

    Style Document List and Document Detail

---
+++ resources/views/include/post/pagination.blade.php
@@ -0,0 +1,17 @@
+<div class="_pagination col-xs-12 col-sm-12 col-md-12">
+<nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <li class="page-item first"><a class="" href="#"><< Đầu</a></li>
+        <li class="page-item previous"><a class="" href="#">< Trước</a></li>
+        <li class="normal">...</li>
+        <li class="page-item"><a class="" href="#">7</a></li>
+        <li class="page-item active"><a class="" href="#">8</a></li>
+        <li class="page-item"><a class="" href="#">9</a></li>
+        <li class="page-item"><a class="" href="#">10</a></li>
+        <li class="page-item"><a class="" href="#">11</a></li>
+        <li class="normal">...</li>
+        <li class="page-item next"><a class="" href="#">Sau ></a></li>
+        <li class="page-item last"><a class="" href="#">Cuối >></a></li>
+    </ul>
+</nav>
+</div>
